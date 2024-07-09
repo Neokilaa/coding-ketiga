af8507cad87f8d5bbff151e82993a33500e90fa4
I Gede Putu Sugiatmika <igedeputusugiatmika@Is-MacBook-Pro.local>
2024-07-09 23:38:51 +0700

    first commit

---
+++ hi.py
@@ -0,0 +1,36 @@
+number = 3
+pi = 3,14
+hari = "selasa"
+salah = False
+
+#list
+minuman = ["cocacola", "indomilk", "nescafe"]
+print(minuman)
+print(minuman[0])
+print(minuman[-1])
+
+#list 2 dimensi
+film = [['lovely runner', 1], ['dilan', 2]]
+print(film[1])
+
+# dictionary dalam list
+coba = [{"nama" : "kayla" , "umur" : 17}]
+print(coba)
+
+# tuple
+laptop = ("apple", "lenovo")
+print(laptop[0])
+
+# list dalam dictionary
+peserta = {"nama" : "kayla" , "umur" : 17, "favorit" : ["matcha" , 'vanilla', 'melon']}
+print(peserta)
+print(peserta["umur"])
+
+# range
+jarak = range(0, 10)
+print(jarak)
+
+#for loop
+for i in range(0, 10): 
+    print(i , end = " menit")
+print()
